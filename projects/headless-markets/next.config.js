d1e965ae574fac0d9aa4ef37b8b17739fdaeaa2e
dutchiono <86275975+dutchiono@users.noreply.github.com>
2026-02-19 22:03:23 -0500

    feat(headless-markets): add next.config.js [issue #18]

--- projects/headless-markets/next.config.js
+++ projects/headless-markets/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  poweredByHeader: false,
+  output: 'export',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
